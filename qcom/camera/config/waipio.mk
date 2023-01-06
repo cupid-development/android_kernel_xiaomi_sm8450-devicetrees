c2b26593275f963fd4a1143ee7e2bcbe05ccdf5b
Arian <arian.kulmer@web.de>
2023-01-06 23:35:47 +0100

    camera: Add support for cupid and zeus
    Change-Id: If67eff9bb354fb11f1e4078cc2f3e1a16755256a

--- qcom/camera/config/waipio.mk
+++ qcom/camera/config/waipio.mk
@@ -2,7 +2,9 @@ dtbo-$(CONFIG_ARCH_WAIPIO) := waipio-camera.dtbo
 dtbo-$(CONFIG_ARCH_WAIPIO) += waipio-camera-overlay-v2.dtbo \
 								waipio-camera-sensor-mtp.dtbo \
 								waipio-camera-sensor-cdp.dtbo \
-								waipio-camera-sensor-qrd.dtbo
+								waipio-camera-sensor-qrd.dtbo \
+								zeus-sm8450-camera-sensor.dtbo \
+								cupid-sm8450-camera-sensor.dtbo
 
 dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera.dtbo
 dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera-sensor-idp.dtbo
