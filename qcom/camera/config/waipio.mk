ea59f92fffad7522621b942ea06c611bf897ea48
Arian <arian.kulmer@web.de>
2023-01-06 23:35:47 +0100

    camera: Add support for cupid, marble, mayfly, mondrian, thor, unicorn and zeus
    Change-Id: If67eff9bb354fb11f1e4078cc2f3e1a16755256a

--- qcom/camera/config/waipio.mk
+++ qcom/camera/config/waipio.mk
@@ -2,7 +2,9 @@ dtbo-$(CONFIG_ARCH_WAIPIO) := waipio-camera.dtbo
 dtbo-$(CONFIG_ARCH_WAIPIO) += waipio-camera-overlay-v2.dtbo \
 								waipio-camera-sensor-mtp.dtbo \
 								waipio-camera-sensor-cdp.dtbo \
-								waipio-camera-sensor-qrd.dtbo
+								waipio-camera-sensor-qrd.dtbo \
+								zeus-sm8450-camera-sensor.dtbo \
+								cupid-sm8450-camera-sensor.dtbo
 
 dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera.dtbo
 dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera-sensor-idp.dtbo
@@ -11,9 +13,14 @@ dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera-sensor-qrd.dtbo
 dtbo-$(CONFIG_ARCH_CAPE) += cape-camera.dtbo
 dtbo-$(CONFIG_ARCH_CAPE) += cape-camera-sensor-mtp.dtbo \
 				cape-camera-sensor-cdp.dtbo \
-				cape-camera-sensor-qrd.dtbo
+				cape-camera-sensor-qrd.dtbo \
+				thor-sm8475-camera-sensor.dtbo \
+				mayfly-sm8475-camera-sensor.dtbo \
+				unicorn-sm8475-camera-sensor.dtbo \
+				mondrian-sm8475-camera-sensor.dtbo
 
 dtbo-$(CONFIG_ARCH_CAPE) += ukee-camera-sensor-mtp.dtbo \
 				ukee-camera-sensor-cdp.dtbo \
-				ukee-camera-sensor-qrd.dtbo
+				ukee-camera-sensor-qrd.dtbo \
+				marble-sm7475-camera-sensor.dtbo
 dtbo-$(CONFIG_ARCH_CAPE) += ukee-camera.dtbo
