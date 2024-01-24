d02ac16fd0b43ae87e0e5227401f56835b891047
Arian <arian.kulmer@web.de>
2024-01-24 10:36:26 +0100

    camera-devicetree: Add support for thor, zizha, diting, mayfly, unicorn and mondrian from ziyi-s-oss
    Change-Id: I7ef8a5cd0b5ad7468ed388c6926538d51c058aee

--- qcom/camera/config/waipio.mk
+++ qcom/camera/config/waipio.mk
@@ -14,7 +14,12 @@ dtbo-$(CONFIG_ARCH_CAPE) += cape-camera.dtbo
 dtbo-$(CONFIG_ARCH_CAPE) += cape-camera-sensor-mtp.dtbo \
 				cape-camera-sensor-cdp.dtbo \
 				cape-camera-sensor-qrd.dtbo \
-				mondrian-camera-sensor.dtbo
+				thor-sm8475-camera-sensor.dtbo \
+				zizhan-sm8475-camera-sensor.dtbo \
+				diting-sm8475-camera-sensor.dtbo \
+				mayfly-sm8475-camera-sensor.dtbo \
+				unicorn-sm8475-camera-sensor.dtbo \
+				mondrian-sm8475-camera-sensor.dtbo
 
 dtbo-$(CONFIG_ARCH_CAPE) += ukee-camera-sensor-mtp.dtbo \
 				ukee-camera-sensor-cdp.dtbo \
